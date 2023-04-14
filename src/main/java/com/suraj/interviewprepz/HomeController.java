ac53b098c2f87eccd2242cbd65fc3bd1684a23b7
Suraj-7268 <130667659+Suraj-7268@users.noreply.github.com>
2023-04-14 21:53:07 +0530

    Add files via upload

---
+++ src/main/java/com/suraj/interviewprepz/HomeController.java
@@ -0,0 +1,33 @@
+package com.suraj.interviewprepz;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index()
+	{
+		return "index.jsp";
+	}
+	@RequestMapping("/index")
+	public String Home()
+	{
+		return "index.jsp";
+	}
+	@RequestMapping("/java")
+	public String Java()
+	{
+		return "java.jsp";
+	}
+	@RequestMapping("/interviewtips")
+	public String InterviewTips()
+	{
+		return "interviewtips.jsp";
+	}
+	@RequestMapping("/applyingtips")
+	public String ApplyingTips()
+	{
+		return "applyingtips.jsp";
+	}
+}
+
